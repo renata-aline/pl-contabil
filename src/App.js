c7f6d1b0770d3564723aad1e8127bc35c8c9d7bd
renata-aline <renatalinec@gmail.com>
2023-11-16 16:37:07 -0300

    construindo o componente main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import Header from './components/Header';
+import Main from './components/Main';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App"> 
      <Navbar />
      <Header />
+     <Main />
     </div>
   );
 }
---
+++ src/components/Main/index.js
@@ -0,0 +1,20 @@
+import './index.css'
+
+function Main() {
+
+    return (
+        <div className='main'>
+            <h2>Sobre Nós</h2>
+            <p>Accountant was founded in 1995 and since that time has grown into the largest CPA firm in the area. We are an assertive firm providing
+                our clients a full range of services.
+                Our offices are located in the Intermountain West because
+                this is where we chose to live. However, we serve a wide variety of 
+                clients located throughout the United States and even some European countries.
+            </p>
+        </div>
+
+    )
+
+}
+
+export default Main
